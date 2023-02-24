1f6297d70c8bee9e8efbe8b4ecc1b4d75c0e1e0a
penguinboots <sabrina.ynw@gmail.com>
2023-02-24 13:24:46 -0800

    initial implement getAllProperties

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -105,12 +105,68 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
+  let queryParams = [];
+
+  let queryString =
+    `SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_reviews.property_id`;
+
+  if (options.city) {
+    if (queryParams) {
+      queryString += 'WHERE';
+    } else {
+      queryString += 'AND';
+    }
+    queryParams.push(`%${options.city}%`);
+    queryString += ` city LIKE $${queryParams.length}`;
+  }
+
+  if (options.owner_id) {
+    if (queryParams) {
+      queryString += 'WHERE';
+    } else {
+      queryString += 'AND';
+    }
+    queryParams.push(`${options.owner_id}`);
+    queryString += `WHERE properties.owner_id = $${queryParams.length}`;
+  }
+
+  if (options.minimum_price_per_night) {
+    if (queryParams) {
+      queryString += 'WHERE';
+    } else {
+      queryString += 'AND';
+    }
+    queryParams.push(`${minimum_price_per_night * 100}`);
+    queryString += `WHERE properties.cost_per_night >= $${queryParams.length}`;
+  }
+
+  if (options.maximum_price_per_night) {
+    if (queryParams) {
+      queryString += 'WHERE';
+    } else {
+      queryString += 'AND';
+    }
+    queryParams.push(`${maximum_price_per_night * 100}`);
+    queryString += `WHERE properties.cost_per_night <= $${queryParams.length}`;
+  }
+
+  queryString += `GROUP BY properties.id`;
+
+  if (options.minimum_rating) {
+    queryParams.push(`${minimum_rating}`);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString +=
+    `ORDER BY cost_per_night
+    LIMIT $${queryParams.length}`;
+
   return pool
     .query(
-      `SELECT *
-      FROM properties
-      LIMIT $1`,
-      [limit])
+      queryString, queryParams)
     .then((result) => {
       return result.rows;
     })
